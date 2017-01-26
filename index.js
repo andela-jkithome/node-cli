25a97eb49ba75dc57e04c52181d22950bd62974d
Jeremy Kithome <jeremy.kithome@andela.com>
2017-01-26 17:32:01 +0300

    Add app title and repository check

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ var git         = require('simple-git')();
 var touch       = require('touch');
 var fs          = require('fs');
 var files = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Ginit', { horizontalLayout: 'full' })
+  )
+);
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}
